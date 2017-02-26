7fec465a8e1edb10308429f906bec9b42c24bc90
RustyHoff <RustyHoff@users.noreply.github.com>
2017-02-25 20:26:31 -0600

    Added CRUD functionality Still need to address #TODO for deleteRestaurant and showMenu

---
+++ vagrant/restaurant/database_setup.py
@@ -0,0 +1,40 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    """Class for sql table restaurant"""
+    __tablename__ = "restaurant"
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+
+
+class MenuItem(Base):
+    """Class for sql table menu_item"""
+    __tablename__ = "menu_item"
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey("restaurant.id"))
+    restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+        #returns object data in easily serializable format
+        return {
+            'name' : self.name,
+            'description' : self.description,
+            'id' : self.id,
+            'price' : self.price,
+            'course' : self.course,
+        }
+
+### Insert at end of file ###
+engine = create_engine("sqlite:///restaurantmenu.db")
+Base.metadata.create_all(engine)
